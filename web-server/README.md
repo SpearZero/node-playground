5306fd43b185c6b89028462d09ed0aee2a2defde
SpearZero <spearzero0@gmail.com>
2020-07-18 19:36:25 +0900

    [web-server]
    create web-server using weather-app

---
+++ web-server/README.md
@@ -0,0 +1,12 @@
+> https://www.udemy.com/course/the-complete-nodejs-developer-course-2/
+
+위의 강의 중 세 번째 프로젝트인 web-server를 실습
+- 두 번째 프로젝트인 weather-app을 이용한 프로젝트
+- express를 이용하여 웹 서버 구동
+- 템플릿 엔진으로 handlebar 사용
+- 헤로쿠를 통한 배포 실습
+
+> https://github.com/andrewjmead/node-course-v3-code/tree/master/web-server
+
+강의자의 web-server 리파지토리 주소
+
---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,20 @@
+const request = require('request')
+
+const forecast = (latitude, longitude, callback) => {
+    const url = "http://api.weatherstack.com/current?access_key=액세스키&query=" + latitude + "," + longitude + "&units=f";
+
+    request({ url, json: true }, (error, { body }) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined);
+        } else if (body.error) {
+            callback('Unable to find location', undefined);
+        } else {
+            const current = body.current;
+            callback(undefined,
+                `${current.weather_descriptions[0]}, temperature ${current.temperature}, feelslike ${current.feelslike}`
+            )
+        }
+    });
+}
+
+module.exports = forecast
